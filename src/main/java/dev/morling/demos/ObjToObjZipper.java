a52f0bb22eca691550d036ca365ca5259d84a495
Gunnar Morling <gunnar.morling@googlemail.com>
2024-03-19 14:39:03 +0100

    Simplifying implementation

--- src/main/java/dev/morling/demos/ObjToObjZipper.java
+++ src/main/java/dev/morling/demos/ObjToObjZipper.java
@@ -7,33 +7,27 @@
  */
 package dev.morling.demos;
 
-import java.util.Spliterator;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Iterator;
 import java.util.function.BiFunction;
 import java.util.function.Supplier;
 import java.util.stream.Gatherer;
 import java.util.stream.Stream;
 
-import dev.morling.demos.ObjToObjZipper.ZipperState;
+public record ObjToObjZipper<T1, T2, R>(Stream<T2> other, BiFunction<T1, T2, R> zipperFunction) implements Gatherer<T1, Iterator<T2>, R> {
 
-public record ObjToObjZipper<T1, T2, R>(Stream<T2> other, BiFunction<T1, T2, R> zipperFunction) implements Gatherer<T1, ZipperState<T2>, R> {
+  @Override
+  public Supplier<Iterator<T2>> initializer() {
+    return () -> other.iterator();
+  }
 
-	@Override
-	public Supplier<ZipperState<T2>> initializer() {
-		return () -> new ZipperState<>(other.spliterator());
-	}
+  @Override
+  public Integrator<Iterator<T2>, T1, R> integrator() {
+    return Gatherer.Integrator.ofGreedy((state, element, downstream) -> {
+      if (state.hasNext()) {
+        return downstream.push(zipperFunction.apply(element, state.next()));
+      }
 
-	@Override
-	public Integrator<ZipperState<T2>, T1, R> integrator() {
-		return Gatherer.Integrator.ofGreedy((state, element, downstream) -> {
-			AtomicReference<T2> otherElement = new AtomicReference<>();
-			if(!state.other().tryAdvance(e -> otherElement.set(e))) {
-				return false;
-			}
-			return downstream.push(zipperFunction.apply(element, otherElement.get()));
-		});
-	}
-
-    static record ZipperState<T>(Spliterator<T> other) {
-    }
+      return false;
+    });
+  }
 }
